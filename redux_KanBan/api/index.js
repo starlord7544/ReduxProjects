240c134a9d55408ed334bdaa92a47f915cd419d8
starlord7544 <sidhantsreshtha2003@gmail.com>
2025-04-01 02:49:53 +0530

    switched to atlas from local

--- redux_KanBan/api/index.js
+++ redux_KanBan/api/index.js
@@ -4,13 +4,15 @@ const mongoose = require('mongoose')
 const port = 6969
 
 const url = 'mongodb://localhost:27017/'
+const atlasUrl = 'mongodb+srv://<username>:<db_password>@kanbancluster.rtijayi.mongodb.net/'
+
 
 const userName = "starlord7544"
 const password = "starlord7544"
 
-const db_url = ''
+const db_url = atlasUrl.replace('<username>', userName).replace('<db_password>', password)
 
-mongoose.connect(url).then(() => {
+mongoose.connect(db_url).then(() => {
     console.log('-------------Connected to Database------------')
 })
 
