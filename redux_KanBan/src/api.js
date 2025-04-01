d7aa06b1388b8f733d177f3bdad5302c011ad9be
starlord7544 <sidhantsreshtha2003@gmail.com>
2025-04-01 23:22:17 +0530

    changed local host url to render

--- redux_KanBan/src/api.js
+++ redux_KanBan/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:6969/api/v1',
+    // baseURL: 'http://localhost:6969/api/v1',
+    baseURL: 'https://reduxprojects.onrender.com/api/v1',
 });
 
 export default {
